51802add30652b069ff6cb6e65d36133b402d20a
brayokenya <kiirubrian21@gmail.com>
2022-05-03 23:35:17 +0300

    deployment to heroku

--- app/request.py
+++ app/request.py
@@ -1,16 +1,15 @@
-from app import app
 import urllib.request,json
-from .models import movie
-
-Movie = movie.Movie
+from .models import Movie
 
 # Getting api key
-api_key = app.config['MOVIE_API_KEY']
-
-
+api_key = None
 # Getting the movie base url
-base_url = app.config["MOVIE_API_BASE_URL"]
+base_url = None
 
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['MOVIE_API_KEY']
+    base_url = app.config['MOVIE_API_BASE_URL']
 
 def get_movies(category):
     '''
@@ -34,10 +33,8 @@ def get_movies(category):
 def process_results(movie_list):
     '''
     Function  that processes the movie result and transform them to a list of Objects
-
     Args:
         movie_list: A list of dictionaries that contain movie details
-
     Returns :
         movie_results: A list of movie objects
     '''
@@ -75,3 +72,18 @@ def get_movie(id):
             movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
 
     return movie_object
+
+def search_movie(movie_name):
+    search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key,movie_name)
+    with urllib.request.urlopen(search_movie_url) as url:
+        search_movie_data = url.read()
+        search_movie_response = json.loads(search_movie_data)
+
+        search_movie_results = None
+
+        if search_movie_response['results']:
+            search_movie_list = search_movie_response['results']
+            search_movie_results = process_results(search_movie_list)
+
+
+    return search_movie_results
---
+++ start.sh
@@ -0,0 +1,5 @@
+export MOVIE_API_KEY='d882445304b469ab965a1ae1a487e8bf'
+
+export SECRET_KEY='123456789'
+
+python3.8 manage.py server
